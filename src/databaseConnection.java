998c38e8571fdc4f1df2cd65a16f402afb4fca97
Suresh <sureshh7070@gmail.com>
2021-03-06 12:00:37 +0530

    Initial commit

---
+++ src/databaseConnection.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+import java.sql.*;
+
+/**
+ *
+ * @author sures
+ */
+
+public class databaseConnection {
+    public static Connection getCon()
+            
+           
+    {
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/student","root","");
+            return con;
+        
+        } 
+
+    
+          catch(Exception e)  
+             
+            {
+                return  null;
+            
+            } 
+            
+        }
+    
+    
+}
